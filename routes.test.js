96e495522718ba334f951d4a02464eae3c961a0b
Alan Puglisi <alan.c.puglisi@gmail.com>
2020-08-30 15:43:40 -0500

    finished writing tests

--- routes.test.js
+++ routes.test.js
@@ -3,11 +3,12 @@ const request = require("supertest");
 const app = require("./app");
 const db = require("./db");
 const Book = require("./models/book");
+
 describe("book routes test", function() {
 	beforeEach(async function() {
 		await db.query(`DELETE FROM books`);
 
-		let b1 = await Book.create({
+		const b1 = await Book.create({
 			isbn: "test",
 			amazon_url: "http://a.co/eobPtX2",
 			author: "test author",
@@ -62,6 +63,83 @@ describe("book routes test", function() {
 					title: "test book2",
 					year: 2017
 				}
+			});
+		});
+		test("cannot create a book without proper input", async function() {
+			let resp = await request(app).post("/books/").send({
+				isbn: "test2",
+				amazon_url: "http://a.co/eobPtX2",
+				author: "test author2",
+				language: "english",
+				pages: 100,
+				publisher: "test pub2",
+				title: "test book2"
+			});
+			expect(resp.status).toEqual(400);
+		});
+	});
+
+	describe("GET /books/:isbn", function() {
+		test("can get book details", async function() {
+			let resp = await request(app).get(`/books/test`);
+			expect(resp.status).toEqual(200);
+			expect(resp.body).toEqual({
+				book: {
+					isbn: "test",
+					amazon_url: "http://a.co/eobPtX2",
+					author: "test author",
+					language: "english",
+					pages: 100,
+					publisher: "test pub",
+					title: "test book",
+					year: 2017
+				}
+			});
+		});
+	});
+	describe("PUT /books/:isbn", function() {
+		test("can edit book details", async function() {
+			let resp = await request(app).put(`/books/test`).send({
+				amazon_url: "http://a.co/eobPtX2",
+				author: "new author",
+				language: "french",
+				pages: 200,
+				publisher: "new pub",
+				title: "new title",
+				year: 2010
+			});
+			expect(resp.status).toEqual(200);
+			expect(resp.body).toEqual({
+				book: {
+					isbn: "test",
+					amazon_url: "http://a.co/eobPtX2",
+					author: "new author",
+					language: "french",
+					pages: 200,
+					publisher: "new pub",
+					title: "new title",
+					year: 2010
+				}
+			});
+		});
+		test("cannot edit book without correct input", async function() {
+			let resp = await request(app).put(`/books/test`).send({
+				amazon_url: "http://a.co/eobPtX2",
+				author: "new author",
+				language: "french",
+				pages: 200,
+				publisher: "new pub",
+				title: "new title"
+			});
+			expect(resp.status).toEqual(400);
+		});
+	});
+	describe("DELETE /books/:isbn", function() {
+		test("can delete book", async function() {
+			let resp = await request(app).delete(`/books/test`);
+			expect(resp.status).toEqual(200);
+			expect(resp.body).toEqual({
+				message: "Book deleted"
 			});
 		});
 	});
